16dd15e0a6c6fe8102aa7fef3a008e382597b62b
des <davisds1@rose-hulman.edu>
2020-03-28 14:11:35 -0400

    client.cpp works with  original c code

--- src/chatter.cpp
+++ src/chatter.cpp
@@ -7,21 +7,24 @@
   #include <pthread.h>
 #include <netdb.h>
 
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
 
 
-
-public class Chatter{
-
-
+  #define PORT 4567
+  #define BUF_SIZE 256
+  #define CLADDR_LEN 100
+using boost::asio::ip::tcp;
+class Chatter{
 
     void error(char *msg)
     {
         perror(msg);
         exit(1);
     }
-
+    
     void * receiveMessage(void * socket) {
-        int sockfd, ret;
+        int  sockfd,ret;
         char buffer[BUF_SIZE];
         sockfd = (int) socket;
 
@@ -40,47 +43,6 @@ public class Chatter{
     }
 
 
-    int main (int argc, char* argv[])
-{
-int sockfd, portno, n;
-struct sockaddr_in serv_addr;
+    
 
-struct hostent* server;
-char buffer[256];
-if (argc < 3){
-fprintf(stderr,"usage %s hostname port\n",argv[0]);
-exit(0);
-}
-fprintf(stderr,"usage %s hostname port\n",argv[1]);
-portno = atoi(argv[2]);
-sockfd = socket(AF_INET, SOCK_STREAM,0);
-printf("made socket with fd %d\n",sockfd);
-if (sockfd <0)
-        error("ERROR opening socket");
-server = gethostbyname(argv[1]);
-if (server == NULL){
-fprintf(stderr,"ERROR, no such host\n");
-exit(0);
-}
-bzero((char*) &serv_addr, sizeof(serv_addr));
-serv_addr.sin_family=AF_INET;
-bcopy((char*)server->h_addr, (char*)&serv_addr.sin_addr.s_addr, server->h_length);
-serv_addr.sin_port= htons(portno);
-if (connect(sockfd,(struct sockaddr*)&serv_addr, sizeof(serv_addr))<0)
-        error("ERROR connecting");
-printf("Please enter the message: ");
-bzero(buffer,256);
-fgets(buffer,255,stdin);
-n = write(sockfd,buffer,strlen(buffer));
-if (n<0)
-        error("ERROR writing to socket");
-bzero(buffer,256);
-n = read(sockfd, buffer, 255);
-if (n<0)
-        error("ERROR reading from socket");
-printf("%s\n",buffer);
-return 0;
-}
-
-
-}
+};
