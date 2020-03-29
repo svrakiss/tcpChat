2a10875c9659b47853da49b6d8a075d809f61602
des <davisds1@rose-hulman.edu>
2020-03-29 11:11:58 -0400

    makefile (works only on my computer )

--- includes/chatter.h
+++ includes/chatter.h
@@ -1,6 +1,11 @@
+#include <boost/asio.hpp>
+using boost::asio::ip::tcp;
 typedef boost::shared_ptr<tcp::socket> sockPtr;
 
 class Chatter {
+    private:
+    sockPtr mySock_;
+    std::string userName;
     public:
     Chatter(sockPtr,std::string);
     Chatter(sockPtr);
--- src/chatter.cpp
+++ src/chatter.cpp
@@ -2,7 +2,7 @@
 #include <thread>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
-
+#include "chatter.h"
 #define PORT 4567
 #define BUF_SIZE 256
 #define CLADDR_LEN 100
@@ -10,41 +10,35 @@ using boost::asio::ip::tcp;
 using namespace std;
 typedef boost::shared_ptr<tcp::socket> sockPtr;
 
-class Chatter
+Chatter::Chatter(sockPtr socket) : mySock_(socket), userName("Bob")
 {
-private:
-    boost::shared_ptr<tcp::socket> mySock_;
-    string userName;
+}
+Chatter::Chatter(sockPtr socket, string name) : mySock_(socket), userName(name) {}
+Chatter::~Chatter()
+{
+    mySock_.reset();
+}
+void Chatter::read()
+{
+    boost::array<char, 256> buf;
+    boost::system::error_code error;
+    size_t len = boost::asio::read(*mySock_, boost::asio::buffer(buf), error);
+    std::cout.write(buf.data(), len);
+    if (error == boost::asio::error::eof)
+        std::cout << "time to die" << std::endl; // Connection closed cleanly by peer.
+    else if (error)
+        throw boost::system::system_error(error); // Some other error.
+}
+void Chatter::write()
+{
+    string buf;
+    getline(cin, buf);
+    boost::system::error_code error;
+    boost::asio::write(*mySock_, boost::asio::buffer(buf), error);
+    if (error == boost::asio::error::eof)
+        std::cout << "time to die" << std::endl; // Connection closed cleanly by peer.
+    else if (error)
+        throw boost::system::system_error(error); // Some other error.
+}
 
-public:
-    Chatter(sockPtr socket) : mySock_(socket), userName("Bob")
-    {
-    }
-    Chatter(sockPtr socket, string name) : mySock_(socket), userName(name) {}
-    ~Chatter()
-    {
-        mySock_.reset();
-    }
-    void read()
-    {
-        boost::array<char, 256> buf;
-        boost::system::error_code error;
-        size_t len = boost::asio::read(*mySock_, boost::asio::buffer(buf), error);
-        std::cout.write(buf.data(), len);
-        if (error == boost::asio::error::eof)
-            std::cout << "time to die" << std::endl; // Connection closed cleanly by peer.
-        else if (error)
-            throw boost::system::system_error(error); // Some other error.
-    }
-    void write()
-    {
-        string buf;
-        getline(cin, buf);
-        boost::system::error_code error;
-        boost::asio::write(*mySock_, boost::asio::buffer(buf), error);
-        if (error == boost::asio::error::eof)
-            std::cout << "time to die" << std::endl; // Connection closed cleanly by peer.
-        else if (error)
-            throw boost::system::system_error(error); // Some other error.
-    }
-};
+;
